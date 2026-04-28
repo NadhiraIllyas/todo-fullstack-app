d7e4bb2a4da5a576bae105ae13755a782f2fb2c1
Naadhira Illyas <fathimanadhira2001@gmail.com>
2026-04-28 19:43:16 +0530

    Modified Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -142,7 +142,7 @@ function App() {
       <div className="w-full max-w-2xl">
 
         <h1 className="text-3xl font-bold text-center mb-4">
-          📝 TODO App
+          TODO Application
         </h1>
 
         {/* INPUT / EDIT */}
